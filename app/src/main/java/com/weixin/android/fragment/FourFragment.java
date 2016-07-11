8116db044cef733b22a4476ac3e6ea990ae7c8a7
godliness <1229727237@qq.com>
2016-07-11 17:34:06 +0800

    perfect aboutme fragemnt

--- app/src/main/java/com/weixin/android/fragment/FourFragment.java
+++ app/src/main/java/com/weixin/android/fragment/FourFragment.java
@@ -5,13 +5,16 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.weixin.android.R;
 
 /**
  * Created by sujizhong on 16/7/11.
  */
-public class FourFragment extends AppBaseFragment {
+public class FourFragment extends AppBaseFragment implements View.OnClickListener {
+
+    private RelativeLayout mRelaPhoto;
 
     @Nullable
     @Override
@@ -23,5 +26,26 @@ public class FourFragment extends AppBaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        View v = getView();
+        mRelaPhoto = (RelativeLayout) v.findViewById(R.id.rela_photo);
+        mRelaPhoto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.rela_photo:
+                break;
+
+            default:
+                break;
+
+        }
+
     }
 }
